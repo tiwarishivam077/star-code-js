9823fa823bb0cd732313779cb6d95e07c6d562b1
Shivam Tiwari <shivam.tiwari@yuma.energy>
2024-08-07 15:55:27 +0530

    __proto__ object

--- topic-wise/18-prototypal-inheritance.js
+++ topic-wise/18-prototypal-inheritance.js
@@ -2,41 +2,41 @@
 
 // Inheritance is like ==> one object trying to access methods and properties of other object
 
-let arr=['Shivam', 'Sai']
+let arr = ['Shivam', 'Sai'];
 
-const a='200'
-const b='3'
-console.log(b/2)
-console.log(a-b+10*b/2)
-// when you type 'arr.' on console in dev tools ==> you see so many methods 
+const a = '200';
+const b = '3';
+console.log(b / 2);
+console.log(a - b + (10 * b) / 2);
+// when you type 'arr.' on console in dev tools ==> you see so many methods
 // ==> where do they come from ? we didn't define them
 
-let object ={
-    name:'shivam',
-    city:'BLR',
-    intro(){
-        console.log(this.name+ ' '+ this.city)
-    }
-}
+let object = {
+  name: 'shivam',
+  city: 'BLR',
+  intro() {
+    console.log(this.name + ' ' + this.city);
+  },
+};
 
-function play(){
-    console.log('play')
+function play() {
+  console.log('play');
 }
-// similarly this object is getting access to other properties and methods 
+// similarly this object is getting access to other properties and methods
 // such as toString function ==>  there is something known as prototype
 
-// whenever you create an object or an array or any functions or a variable in js, 
+// whenever you create an object or an array or any functions or a variable in js,
 // js engine  automatically puts (some hidden properties and methods) into an obejct
-//  and then attaches this object to your object and that is how you get access to 
+//  and then attaches this object to your object and that is how you get access to
 // those properties and methods
 
 // we can access these properties by writing ==> arr.__proto__.'****' => **** = func name
 // similarly for objects and function also
 
-// basically porotype object is attached to every object, method, array, func
+// basically prototype object is attached to every object, method, array, func
 // properties of this object can be acessed directly by using dot operator
 
-// arr.__proto__  ..? 
+// arr.__proto__  ..?
 
 // prototype chaining ..?
 
@@ -49,95 +49,85 @@ function play(){
 // 6) similarly for function ==> play.__proto__ = Function.prototype
 // 7) play.__proto__ .__proto__ = Object.prototype
 
-
 // from here we can say that everything in js is nothing but an object
 // as object prototype is attached to everything in the js
 
-
-
 let object2 = {
-    // name:'Satyam'
-}
+  // name:'Satyam'
+};
 
 // Never do this in real life   `coding ==>      directly modifying prototype of object2
-object2.__proto__=object
+object2.__proto__ = object;
 // now proto of object2 is pointing to the object defined at the top
 // now object2 can access all the properties of object
 // try in console ==> object2.name, object2.city, object2.intro()
 
-// this is how object2 inherits the object 
+// this is how object2 inherits the object
 // this is what prototypal inheritance is
 
-console.log(object2.intro())
-
+console.log(object2.intro());
 
 // similarly we can atach some common function to the Function prototype
 // for e.g.
 
-Function.prototype.cricket=()=>{
-    console.log('India wins 2023 world cup')
-}
+Function.prototype.cricket = () => {
+  console.log('India wins 2023 world cup');
+};
 
-function fun(){
+function fun() {}
 
-}
-
-fun.cricket()
-
+fun.cricket();
 
 // read about HasOwnProperty function in object prototype
 
-console.log(object.hasOwnProperty('name')) 
+console.log(object.hasOwnProperty('name'));
 // ==> checks whether a property is present in an object or not
 
-
-function Human(){
-    this.lifeSpan = 100
- }
- 
- Human.prototype.breathe = function(){
-     console.log('I am breathing...')
- }
-
- let rohan = new Human()
-
- console.log(rohan.breathe(), '******************')
-
- 
-// constructor function
-function Profile(n,a,o,h ){
-   this.name=n,
-   this.age = a,
-   this.occupation = o,
-   this.address = h
-   this.intro=()=>{
-    console.log( this.name + ' '+ this.age + ' ' + this.occupation + ' ' + this.address)
-   }
+function Human() {
+  this.lifeSpan = 100;
 }
 
+Human.prototype.breathe = function () {
+  console.log('I am breathing...');
+};
 
-let Shubham = new Profile('shubham', '26', 'backend', 'banaras') 
-let Rohit = new Profile('Rohit', '26', 'data', 'katihar' )
-console.log(Shubham, Rohit)
+let rohan = new Human();
 
+console.log(rohan.breathe(), '******************');
+
+// constructor function
+function Profile(n, a, o, h) {
+  (this.name = n), (this.age = a), (this.occupation = o), (this.address = h);
+  this.intro = () => {
+    console.log(
+      this.name + ' ' + this.age + ' ' + this.occupation + ' ' + this.address
+    );
+  };
+}
+
+let Shubham = new Profile('shubham', '26', 'backend', 'banaras');
+let Rohit = new Profile('Rohit', '26', 'data', 'katihar');
+console.log(Shubham, Rohit);
 
 // it is important to note that ==> properites in a constructor func
 // is copy pasted in the object ==> properties amy differ for each new object
-// but methods remains the same for each object i.e. intro, there is no need to 
+// but methods remains the same for each object i.e. intro, there is no need to
 // copy and paste intro method in every object as it violates dry concept
 // instead we can declare intro method in constructor function prototype
 
 // Use of prototype
-Profile.prototype.introduction = function(){
-    console.log('I am function attached to prototype,', this.name + ' **** '+ this.age)
-    console.log(this.name + ' **** '+ this.age)
-}
+Profile.prototype.introduction = function () {
+  console.log(
+    'I am function attached to prototype,',
+    this.name + ' **** ' + this.age
+  );
+  console.log(this.name + ' **** ' + this.age);
+};
 
-console.log(Shubham.introduction(), '===>>>')
-
+console.log(Shubham.introduction(), '===>>>');
 
 // how to connect Profile functions prototype to some other prototype
 
 Profile.prototype.__proto__ = Object.create(Human.prototype);
 
-console.log(Rohit.breathe(), 'cnvbskvdsk')
+console.log(Rohit.breathe(), 'cnvbskvdsk');
--- topic-wise/23-class-in-js.js
+++ topic-wise/23-class-in-js.js
@@ -1,133 +1,142 @@
 // ES6 --> Class and Object
 
 class person {
-    // constructor function in class ==> this function is invoked automatically
-    // at the moment an object of above class is created
-    // it is used to initialize the object, set values to properties
+  // constructor function in class ==> this function is invoked automatically
+  // at the moment an object of above class is created
+  // it is used to initialize the object, set values to properties
 
-    constructor(n,a){
-        console.log('person created')
-        this.name =n;
-        this.age=a;
-    }
+  constructor(n, a) {
+    console.log('person created');
+    this.name = n;
+    this.age = a;
+  }
 
-    // method in a class i.e. it is a prototype method  ===> methods get attached to prototype ==> prototype chaining continues
-      sayHi(){
-        console.log('Ram Ram bhaiya..!')
-    }
+  // method in a class i.e. it is a prototype method  ===> methods get attached to prototype ==> prototype chaining continues
+  sayHi() {
+    console.log('Ram Ram bhaiya..!');
+  }
 
-
-    // static method or property ==>it is not called through object rather called through class ==> goto line34
-    //  this function is not attached to object prototype stays in class
-    static sayHello(){
-        console.log('Radhe Radhe..!')
-    }
+  // static method or property ==>it is not called through object rather called through class ==> goto line34
+  //  this function is not attached to object prototype stays in class
+  static sayHello() {
+    console.log('Radhe Radhe..!');
+  }
 }
 
 let Ram = new person('Ram', 28);
 let Mohan = new person();
 
-console.log(Ram, Mohan)   // blank object
+console.log(Ram, Mohan); // blank object
 
-Ram.sayHi()
+Ram.sayHi();
+Mohan.sayHi();
 
-// Ram.sayHello() // ==> throws an error
+// Ram.sayHello() // ==> throws an error, not a function
 
-person.sayHello() // works properly
-
+person.sayHello(); // works properly, static func ==> executed using class
 
 // How to use inheritance in ES6
 
-class Employee{  //(Parent Class)
-    constructor(n){
-        console.log('Employee Constructor invoked...')
-        this.name = n;
-    }
-    msg(){
-        console.log('Khalistani Murdabad')  // inside a class, the method is stored in protoype
-    }                                      // in order to stick to dry concept
+class Employee {
+  //(Parent Class)
+
+  constructor(n) {
+    console.log('Employee Constructor invoked...');
+    this.name = n;
+  }
+  msg() {
+    console.log('Khalistani Murdabad'); // inside a class, the method is stored in protoype
+  } // in order to stick to dry concept
 }
 
-class Manager extends Employee{  // here manager(Child) class is inheriting class Employee props and methods
-    constructor(n, d){
-       super(n)    // super func is used to call constructor func of parent(inherited) class
-       this.department =d;
-    }
-   
-    msg(){
-        console.log('Child class same function created')
-    }
-    info(){
-        super.msg()  // parent class function called using super keyword
-        this.msg()   // function defined in  same class ===> accessed using this keyword
-        console.log(this.name + ' ' + this.department)
-    }
+// console.log(Employee, 'Employee 52');
+
+class Manager extends Employee {
+  // here manager(Child) class is inheriting class Employee props and methods
+  constructor(n, d) {
+    super(n); // super func is used to call constructor func of parent(inherited) class
+    this.department = d;
+  }
+
+  msg() {
+    console.log('Child class same function created');
+  }
+  info() {
+    super.msg(); // parent class function called using super keyword
+    this.msg(); // function defined in  same class ===> accessed using this keyword
+    console.log(this.name + ' ' + this.department);
+  }
 }
 
-let manager1 = new Manager('Shivam', 'web app')
+let manager1 = new Manager('Shivam', 'web app');
 
-console.log(manager1,'*********')  // if you see console it reads ==> Employee Constructor invoked...
+console.log(manager1, '*********'); // if you see console it reads ==> Employee Constructor invoked...
 // we have not created any object of class Employee but Manager, still constructor func of
 // class Employee invoked ==> it is bcoz manager class extends employees class
 
 //  if we want to access the msg() method inside class manager ==> we will again use super keyword
 // for. e.g.  use ==> super.msg()
 
+manager1.info(); //
+class Admin extends Manager {}
 
-class Admin extends Manager{
- 
-}
+let admin = new Admin('Sai', 'DevOps');
 
-let admin = new Admin('Sai', 'DevOps')
-
-console.log(admin, 'admin')
-
+console.log(admin, 'admin');
 
 // Public and private Property
 
-
 class student {
+  #girlfriendName = 'Jennie';
 
+  constructor(n, c, d, h) {
+    this.name = n;
+    this.class_year = c;
+    this.discipline = d;
+    this.hostel = h;
+  }
 
-     #girlfriendName = 'Jennie';
+  getName() {
+    console.log(
+      this.name +
+        ' ' +
+        this.class_year +
+        ' ' +
+        this.discipline +
+        ' ' +
+        this.hostel
+    );
+  }
 
-    constructor(n, c, d,h){
-     this.name = n
-     this.class_year = c
-     this.discipline = d
-     this.hostel = h 
-    }
+  girlFriend() {
+    // now this function has become pvt, can be accessed inside this class only
+    console.log(this.#girlfriendName, ' I am the  girlFriend');
+  }
 
-    getName(){
-        console.log(this.name + ' ' + this.class_year + ' ' + this.discipline + ' '  + this.hostel)
-    }
+  #secret() {
+    console.log('Gazab Chootiya Kata tha');
+  }
 
-    girlFriend(){    // now this function has become pvt, can be accessed inside this class only
-        console.log(this.#girlfriendName, " I am the  girlFriend")
-    }
-
-    #secret(){
-        console.log('Gazab Chootiya Kata tha')
-    }
-
-    details(){
-        console.log('Lovely Couple, everyone\'s favourtie,')
-        console.log('Rohit mad in Love, Jennie gets sexual favours from other boys too')
-        this.#secret()
-    }
+  details() {
+    console.log("Lovely Couple, everyone's favourtie,");
+    console.log(
+      'Rohit mad in Love, Jennie gets sexual favours from other boys too'
+    );
+    this.#secret();
+  }
 }
 
-let Rohit = new student('Rohit', '2019', 'CST', 'Barak')
+let Rohit = new student('Rohit', '2019', 'CST', 'Barak');
 
-console.log(Rohit)   //so all the public properties are consoled in dev tools
+console.log(Rohit); //so all the public properties are consoled in dev tools
 
 // we can access public properties and methods in this manner ==> public properties can directly be
 // accessed using object
-Rohit.getName()
+Rohit.getName();
 
 //  but private properties  can only be accessed inside class and they are defined in the following manner
 
-//  give # tag to the property you want to make private, 
+//  give # tag to the property you want to make private,
 // and it is to be defined outside constructor function in the class
 
 //  we have defined a pvt property named girlfreindName. let's try to access it outside class
@@ -137,48 +146,46 @@ Rohit.getName()
 // but we can print girlfriendName by using it inside class only for e.g. let's define a func
 //  inside class where this pvt property is used
 
-Rohit.girlFriend()  // ==> see in console ==> gf's name
+Rohit.girlFriend(); // ==> see in console ==> gf's name
 
 // Rohit.friend()
 
-Rohit.details()
-
-
+Rohit.details();
 
 // Mixing ==> important concept  ==> how to access these methods via an object of some class, note the methods below are defined
 //  inside object, not a class==> hence it cannot be extended as usual to that class
 
-let otherUsefulMethods={
-    getReminder(x, y){
-        return(x%y)
-    },
+let otherUsefulMethods = {
+  getReminder(x, y) {
+    return x % y;
+  },
 
-    getProduct(x, y){
-    return  x*y
-    },
+  getProduct(x, y) {
+    return x * y;
+  },
 
-    printStars(){
-        console.log('* ** *** ***** ')
-    }
+  printStars() {
+    console.log('* ** *** ***** ');
+  },
+};
+
+class user {
+  constructor(name) {
+    this.user = name;
+  }
 }
 
-class user{
-    constructor(name){
-        this.user =name
-    }
-}
-
-let temp= new user('temp')
+let temp = new user('temp');
 
 // console.log(temp.getReminder())   // this line gives an error ==> get reminder is not a function
 
-//  so how to access getReminder inside temp ==> follow this method ==> 
+//  so how to access getReminder inside temp ==> follow this method ==>
 
 // Object.assign(user.prototype, otherUsefulMethods)  //==> these methods are now attached to user object prototype
 
 // console.log(temp)   // see in console, in temp's protype==> otherUsefulMethods are attached
 
-temp.__proto__=otherUsefulMethods  // method 2
-console.log(temp)    // same result
+temp.__proto__ = otherUsefulMethods; // method 2
+console.log(temp); // same result
 
 // temp.printStars()  // see in console starts are printed
